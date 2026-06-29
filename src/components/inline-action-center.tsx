b2f29397e7cb845552eedb5be05b6d0a1c7c6d7e
mulyanto88-cyber <mulyanto.my88@gmail.com>
2026-06-29 20:41:57 +0700

    Delete src directory

--- src/components/inline-action-center.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const ActionCenter = dynamic(() => import('./action-center'), { ssr: false })
-
-export default function InlineActionCenter() {
-  return <ActionCenter />
-}
