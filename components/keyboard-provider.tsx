a760fa93fab381874c00eb71d092c5d6c821e1d5
mulyanto88-cyber <mulyanto.my88@gmail.com>
2026-06-29 20:40:33 +0700

    Delete components directory

--- components/keyboard-provider.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { useKeyboardNav } from './use-keyboard-nav'
-
-export default function KeyboardProvider({ children }: { children: React.ReactNode }) {
-  useKeyboardNav()
-  return <>{children}</>
-}
